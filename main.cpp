9508140b0ed48fed358194dff98a22ad71c6cb8c
NormalMood <littlebigdiamond@gmail.com>
2024-03-05 19:23:05 +0300

    feat: added initMatrix func

--- main.cpp
+++ main.cpp
@@ -14,6 +14,14 @@ void print_matrix(char* text, int matrix[N][N]) {
     printf ("\n\n");
 }
 
+void initMatrix(int matrix[N][N]) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matrix[i][j] = rand() % 21 - 10;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     return 0;
 }
