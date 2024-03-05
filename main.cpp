894f44fef670884a20046ce6dfff5e3935692081
NormalMood <littlebigdiamond@gmail.com>
2024-03-05 19:21:13 +0300

    feat: added print_matrix method

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+#define N 4
+#include "mpi.h"
+#include <ctime>
+#include <iostream>
 
+void print_matrix(char* text, int matrix[N][N]) {
+    std::cout << text << std::endl;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            printf(" %d", a[i][j]);
+        }
+        printf ("\n");
+    }
+    printf ("\n\n");
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
